f683d5c9060fdc371a272a1c28a48f14fc229dc0
derysudrajat <dery.sudrajat17@gmail.com>
2020-08-13 22:21:59 +0700

    1. add library stfalcon-imageviewer 2. add full screen image view in DetailPhotosAdapter

--- app/src/main/java/id/derysudrajat/dolan/adapter/DetailPhotosAdapter.kt
+++ app/src/main/java/id/derysudrajat/dolan/adapter/DetailPhotosAdapter.kt
@@ -6,11 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import coil.api.load
+import com.stfalcon.imageviewer.StfalconImageViewer
 import id.derysudrajat.dolan.R
 import id.derysudrajat.dolan.databinding.ItemPhotoBinding
 
 class DetailPhotosAdapter(
-    val context: Context,
+    private val context: Context,
     private val listPhotos: MutableList<String>
 ) : RecyclerView.Adapter<DetailPhotosAdapter.DetailPhotosViewHolder>() {
     private lateinit var binding: ItemPhotoBinding
@@ -20,14 +21,17 @@ class DetailPhotosAdapter(
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailPhotosViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_photo, parent, false)
         binding = ItemPhotoBinding.bind(view)
-        return DetailPhotosViewHolder(
-            view
-        )
+        return DetailPhotosViewHolder(view)
     }
 
     override fun getItemCount(): Int = listPhotos.size
 
     override fun onBindViewHolder(holder: DetailPhotosViewHolder, position: Int) {
         binding.ivPhoto.load(listPhotos[position]) { crossfade(true) }
+        binding.contentImage.setOnClickListener {
+            StfalconImageViewer.Builder<String>(context, listPhotos) { imageView, image ->
+                imageView.load(image) { crossfade(true) }
+            }.withStartPosition(position).show()
+        }
     }
 }
